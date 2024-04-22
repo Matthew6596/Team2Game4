3a20aa47aeb432df2268c9743f7bdb7868d5f311
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-04-22 18:40:31 -0400

    Began menu stuff

--- team2game4/Assets/Scripts/MenuScript.cs
+++ team2game4/Assets/Scripts/MenuScript.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static string prevScene;
+    public static void ChangeScene(string name)
+    { 
+        prevScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(name); 
+    }
+    public static void ChangeScene(int index)
     {
-        
+        prevScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(index); 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
