8f9a85ec9631205b1be2fffdcca3e41d840a0255
rft0202 <rftester@email.neit.edu>
2024-04-23 15:56:06 -0400

    still working on player movement

--- team2game4/Assets/Scripts/PlayerMovement.cs
+++ team2game4/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -21,6 +22,19 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump()
     {
-        gameObject.transform.position = target.transform.position;
+        Vector3 targetPos = target.transform.position;
+        Vector3 tempPos = gameObject.transform.position;
+        /*{
+            tempPos.x++;
+            if(target.transform.position.y > gameObject.transform.position.y)
+            {
+                tempPos.y++;
+            }
+            else
+            {
+                tempPos.y--;
+            }
+            gameObject.transform.position = tempPos;
+        } while (tempPos.x != targetPos.x) ; //infinite loop whoops */
     }
 }
