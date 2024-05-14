e1172a60915a10c0e77475d45c54ead26fa277b4
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-05-14 19:42:55 -0400

    Countdown size tween

--- team2game4/Assets/Scripts/StartCountdown.cs
+++ team2game4/Assets/Scripts/StartCountdown.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
 
 public class StartCountdown : MonoBehaviour
 {
@@ -18,15 +19,21 @@ public class StartCountdown : MonoBehaviour
         playerControl.enabled = false;
         countdownTxt = GetComponent<TMP_Text>();
         StartCoroutine(countDown());
+        transform.localScale = Vector3.one*2f;
     }
 
+    private void Update()
+    {
+        transform.localScale = Tween.LazyTween(transform.localScale, Vector3.one, 0.1f);
+    }
     IEnumerator countDown()
     {
         yield return new WaitForSeconds(0.75f);
         cnt--;
         countdownTxt.text = (cnt==0)?("GO!"):cnt.ToString();
+        transform.localScale = Vector3.one * 2f;
 
-        if(cnt>=0) StartCoroutine(countDown());
+        if (cnt>=0) StartCoroutine(countDown());
         else EndCountdown();
     }
 
